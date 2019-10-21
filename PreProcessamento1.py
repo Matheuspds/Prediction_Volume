dc50aec4b5966680113d0c4613451e72e010ce72
Matheuspds <matheuspds2@gmail.com>
2019-10-21 20:23:04 -0300

    Adicionando mais features ao modelo, considerando a hora e o dia da semana

--- PreProcessamento1.py
+++ PreProcessamento1.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[34]:
+# In[1]:
 
 
 import pandas as pd
@@ -16,7 +16,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
 
 
-# In[5]:
+# In[2]:
 
 
 # Descrição das features:
@@ -30,14 +30,14 @@ from sklearn.ensemble import AdaBoostRegressor
 # weekend        int       1: Para quando for fim de semana; 0: Para quando não for fim de semana
 
 
-# In[14]:
+# In[15]:
 
 
 volume_df = pd.read_csv("dataset/volume(table 6)_test1.csv")
 volume_df.head()
 
 
-# In[15]:
+# In[5]:
 
 
 #Caracteristicas dos dados
@@ -45,7 +45,7 @@ volume_df.info()
 volume_df.describe()
 
 
-# In[12]:
+# In[16]:
 
 
 #Retirando os valores nulos da coluna vehicle_type pelo modelo do veículo.
@@ -54,25 +54,25 @@ volume_df.describe()
     #Ficou definido que sera para passageiro, sendo maior que 4 será veiculo de carga
 
 
-# In[16]:
-
-
-volume_df['vehicle_type'] = volume_df['vehicle_model'].apply(lambda x: 0 if x < 5 else 1)
-
-
 # In[17]:
+
+
+#volume_df['vehicle_type'] = volume_df['vehicle_model'].apply(lambda x: 0 if x < 5 else 1)
+
+
+# In[18]:
 
 
 volume_df.head()
 
 
-# In[7]:
+# In[19]:
 
 
 volume_df.tail()
 
 
-# In[18]:
+# In[20]:
 
 
 #Função que será usada para obter a janela de tempo de 20 minutos
@@ -94,11 +94,15 @@ def get_hour(t):
         return t.hour
 
 
-# In[19]:
+# In[21]:
 
 
 #Ajustando o formato da coluna time
 volume_df['time'] = pd.to_datetime(volume_df['time'], format = '%Y-%m-%d %H:%M:%S')
+
+#Representam os feriados
+holiday1 = [pd.Timestamp('2016-10-1'), pd.Timestamp('2016-10-8')]
+holiday2 = [pd.Timestamp('2016-9-15'), pd.Timestamp('2016-9-18')]
 
 #Adiciona valores para os dias da semana
 volume_df['weekday'] = volume_df['time'].dt.dayofweek + 1
@@ -113,15 +117,15 @@ volume_df['hour'] = volume_df['t'].apply(get_hour)
 
 volume_df['date'] = volume_df['time'].dt.date
 
+volume_df['holiday'] = volume_df['time'].between(holiday1[0],holiday1[1])                            | volume_df['time'].between(holiday2[0],holiday2[1])
 
 volume_df['time_window'] = volume_df['t'].apply(get_timewindow)
 del volume_df['t']
 
 volume_df.head()
-volume_df.info()
 
 
-# In[20]:
+# In[22]:
 
 
 #Salvando dados de treino
@@ -134,15 +138,15 @@ volume_df.to_csv('processed_test_volume2.csv', index = False)
 #Fazendo o mesmo processo para os dados de teste
 
 
-# In[21]:
+# In[41]:
 
 
 pd_volume_train = pd.read_csv('processed_training_volume2.csv')
 pd_volume_test = pd.read_csv('processed_test_volume2.csv')
 pd_volume_train = pd_volume_train.set_index(['time'])
 pd_volume_test = pd_volume_test.set_index(['time'])
-volume_train = pd_volume_train.groupby(['time_window','tollgate_id','direction','weekday', 'hour']).size().reset_index().rename(columns = {0:'volume'})
-volume_test = pd_volume_test.groupby(['time_window','tollgate_id','direction','weekday', 'hour']).size().reset_index().rename(columns = {0:'volume'})
+volume_train = pd_volume_train.groupby(['time_window','tollgate_id','direction', 'hour', 'date']).size().reset_index().rename(columns = {0:'volume'})
+volume_test = pd_volume_test.groupby(['time_window','tollgate_id','direction', 'hour', 'date']).size().reset_index().rename(columns = {0:'volume'})
 
 x = pd.Series(volume_train['time_window'].unique())
 s = pd.Series(range(len(x)),index = x.values)
@@ -154,47 +158,84 @@ volume_test['window_n'] = volume_test['time_window'].map(s)
 volume_test.head()
 
 
-# In[36]:
+# In[80]:
 
 
 def feature_format():
-    pd_volume_train = pd.read_csv('processed_training_volume2.csv')
+    pd_volume_train = pd.read_csv('processed_train_volume2.csv')
     pd_volume_test = pd.read_csv('processed_test_volume2.csv')
-    pd_volume_train = pd_volume_train.set_index(['time'])
-    pd_volume_test = pd_volume_test.set_index(['time'])
-    volume_train = pd_volume_train.groupby(['time_window','tollgate_id','direction','weekday', 'hour']).size().reset_index().rename(columns = {0:'volume'})
-    volume_test = pd_volume_test.groupby(['time_window','tollgate_id','direction','weekday', 'hour']).size().reset_index().rename(columns = {0:'volume'})
+    #pd_volume_train = pd_volume_train.set_index(['time'])
+    #pd_volume_test = pd_volume_test.set_index(['time'])
+    volume_train = pd_volume_train.groupby(['time_window','tollgate_id','direction','date', 'hour']).size().reset_index().rename(columns = {0:'volume'})
+    volume_test = pd_volume_test.groupby(['time_window','tollgate_id','direction','date', 'hour']).size().reset_index().rename(columns = {0:'volume'})
                     
     x = pd.Series(volume_train['time_window'].unique())
     s = pd.Series(range(len(x)),index = x.values)
     volume_train['window_n'] = volume_train['time_window'].map(s)
     volume_test['window_n'] = volume_test['time_window'].map(s)
-    #volume_test.tail()
+#        print vol_test.tail()
+    volume_train['weekday'] = pd_volume_train['weekday']
+    volume_test['weekday'] = pd_volume_test['weekday']
     
-    feature_train = volume_train.drop('volume', axis = 1).set_index(['time_window'])
-    feature_test = volume_test.drop('volume',axis = 1).set_index(['time_window'])
+    feature_train = volume_train.drop('volume', axis = 1)
+    feature_test = volume_test.drop('volume',axis = 1)
     values_train = volume_train['volume'].values
     values_test = volume_test['volume'].values
     
     return feature_train, feature_test, values_train, values_test
 
 
-# In[37]:
+# In[81]:
 
 
-x_train, x_test, y_train, y_test = feature_format()
+feature_train, feature_test, values_train, values_test = feature_format()
 
 
-# In[24]:
+# In[82]:
 
 
-x_train
+print(feature_train)
 
 
-# In[25]:
+# In[83]:
 
 
-y_train
+feature_train[['window_n', 'tollgate_id', 'direction', 'weekday']]
+
+
+# In[84]:
+
+
+print(values_train)
+
+
+# In[85]:
+
+
+print(feature_test)
+
+
+# In[86]:
+
+
+rng = np.random.RandomState(1)
+regr = AdaBoostRegressor(DecisionTreeRegressor(max_depth = 15),
+                         n_estimators=20, random_state = rng)
+
+
+# In[87]:
+
+
+regr.fit(feature_train[['window_n', 'tollgate_id', 'direction', 'weekday']], values_train)
+
+y_pred = regr.predict(feature_test[['window_n', 'tollgate_id', 'direction', 'weekday']])
+
+mape = np.mean(np.abs((y_pred - values_test)/values_test))
+
+#print (feature_test)
+print (mape)
+
+#print (y_pred,values_test)
 
 
 # In[26]:
